1a27ae74b46d1f718a2050f5da0fefcee5d7ba5c
bryanl <bryan@osesm.com>
2016-01-02 00:44:38 -0500

    update script can handle any unit

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,14 +5,16 @@ if [[ $FLEETCTL_TUNNEL == "" ]]; then
   exit 1
 fi
 
-for i in "$@"; do
-  agent="dolb-agent@$i"
-  echo "restarting ${agent}"
-  fleetctl stop $agent
+name=$1
+
+for i in "${@:2}"; do
+  unit="${name}@$i"
+  echo "restarting ${unit}"
+  fleetctl stop $unit
   sleep 2
-  fleetctl start $agent
+  fleetctl start $unit
   while true; do
-    fleetctl status $agent | grep "active (running)" &> /dev/null
+    fleetctl status $unit | grep "active (running)" &> /dev/null
     if [[ $? == 0 ]]; then
       up=1
       break
