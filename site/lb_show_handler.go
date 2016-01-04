43ac7cac7191c55365106459ba67962fd9a0bb39
bryanl <bryan@osesm.com>
2016-01-03 20:06:53 -0500

    show lb info on page

--- site/lb_show_handler.go
+++ site/lb_show_handler.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	"github.com/bryanl/dolb/dao"
+	"github.com/gorilla/mux"
 )
 
 type LBShowHandler struct {
@@ -19,11 +20,38 @@ func (h *LBShowHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err := renderTemplate(w, "lb_show.tmpl", nil)
+	vars := mux.Vars(r)
+	lbID := vars["lb_id"]
+
+	data, err := h.buildView(lbID)
+	if err != nil {
+		logrus.WithError(err).Error("could not load load balancer")
+		w.WriteHeader(404)
+		fmt.Fprintln(w, "404 page not found")
+		return
+	}
+
+	err = renderTemplate(w, "lb_show.tmpl", data)
 	if err != nil {
 		logrus.WithError(err).Error("could not render template")
 		w.WriteHeader(500)
 		fmt.Fprintln(w, err)
 	}
+}
 
+func (h *LBShowHandler) buildView(id string) (map[string]interface{}, error) {
+	lb, err := h.bh.config.DBSession.LoadLoadBalancer(id)
+	if err != nil {
+		return nil, err
+	}
+
+	agents, err := h.bh.config.DBSession.LoadBalancerAgents(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return map[string]interface{}{
+		"LoadBalancer": lb,
+		"Agents":       agents,
+	}, nil
 }
