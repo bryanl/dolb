ed35bc5448ddc69cf4d4c9ed08542c8ffe4b2ef5
bryanl <bryan@osesm.com>
2016-01-05 13:06:10 -0500

    don't pop a new browser when grunt starts

--- site/Gruntfile.js
+++ site/Gruntfile.js
@@ -75,7 +75,6 @@ module.exports = function (grunt) {
       },
       livereload: {
         options: {
-          open: true,
           middleware: function (connect) {
             return [
               connect.static('.tmp'),
