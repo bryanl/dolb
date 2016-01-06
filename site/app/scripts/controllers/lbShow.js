f8e80bed28dd39019bf1957a3ad6ab5b06cf8779
bryanl <bryan@osesm.com>
2016-01-06 17:11:09 -0500

    preliminary support for deleting a load balancer

--- site/app/scripts/controllers/lbShow.js
+++ site/app/scripts/controllers/lbShow.js
@@ -2,21 +2,30 @@
   'use strict';
 
   angular.module('siteApp')
-    .controller('LBShowCtrl', function (session, $scope, $routeParams, $http) {
+    .controller('LBShowCtrl', function (session, $scope, $routeParams, $http, $location) {
       session.then(function() {
         $scope.lbID = $routeParams.lbid;
         $scope.lb = {};
 
         var u = '/api/lb/' + $scope.lbID;
-        console.log('fetching: ' + u);
         $http.get(u)
           .success(function(res) {
-            console.log(res);
             $scope.lb = res;
           })
           .error(function(res) {
             console.log('error: ' + JSON.stringify(res));
           });
+
+        $scope.deleteLB = function() {
+          var u = '/api/lb/' + $scope.lbID;
+          $http.delete(u)
+            .success(function() {
+              $location.path('/');
+            })
+            .error(function(data) {
+              console.log('lb delete failed: ' + JSON.stringify(data));
+            });
+        };
       });
     });
 
