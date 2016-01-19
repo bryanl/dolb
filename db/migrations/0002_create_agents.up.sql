4c449a18c77fdd635869c71ca8abf059cd0004ea
bryanl <bryan@osesm.com>
2016-01-18 21:55:52 -0500

    update agent schema with new fields

--- db/migrations/0002_create_agents.up.sql
+++ db/migrations/0002_create_agents.up.sql
@@ -1,9 +1,10 @@
 CREATE TABLE agents (
 	id text PRIMARY KEY,
 	cluster_id text,
+	region text,
 	droplet_id integer NOT NULL DEFAULT 0,
-	name text NOT NULL,
-	ip_id integer NOT NULL DEFAULT 0,
+	droplet_name text NOT NULL,
+	dns_id integer NOT NULL DEFAULT 0,
 	last_seen_at TIMESTAMP NOT NULL DEFAULT NOW(),
 	is_deleted boolean DEFAULT false
 );
